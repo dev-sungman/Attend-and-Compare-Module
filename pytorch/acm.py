01a0f1960d53df6ffba1d4fce8a863c886ac5b4b
Sungman-Cho <pccho9108@gmail.com>
2020-08-13 10:34:58 +0900

    update acm block

--- pytorch/acm.py
+++ pytorch/acm.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class ACModule(nn.Module):
+class ACMBlock(nn.Module):
     def __init__(self, in_channels, groups=1):
         super(ACModule, self).__init__()
 
@@ -13,15 +13,20 @@ class ACModule(nn.Module):
     
         self.k_conv = nn.Sequential(
             nn.Conv2d(self.in_channels, self.out_channels, (1,1), groups=self.groups),
-            nn.Softmax()
+            nn.Softmax(dim=1)
         )
 
         self.q_conv = nn.Sequential(
             nn.Conv2d(self.in_channels, self.out_channels, (1,1), groups=self.groups),
-            nn.Softmax()
+            nn.Softmax(dim=1)
         )
 
-        self.global_pooling = nn.Conv2d(self.in_channels, self.out_channels, (1,1), groups=self.groups)
+        self.global_pooling = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.out_channels, (1,1), groups=self.groups),
+            nn.ReLU(),
+            nn.Conv2d(self.in_channels, self.out_channels, (1,1), groups=self.groups),
+            nn.Sigmoid()
+        )
 
     def _normalize_features(self, x):
         # get mean vecotr by axis C
@@ -31,36 +36,27 @@ class ACModule(nn.Module):
         c_mean = torch.mean(x, 1) 
         return c_mean
 
-    def _select_features(self):
-        print('TODO')
-        
-
 
     def forward(self, x):
         mean_features = self._normalize_features(x)
         normalized_features = x - mean_features
-        
 
         K = self.k_conv(x)
         Q = self.q_conv(x)
+        
+        K = torch.matmul(K, x)
+        Q = torch.matmul(Q, x)
+        
+        diff = K-Q
+        channel_weight = self.global_pooling(x)
 
-        print(K)
-        print(Q)
-
-
-
-
-
-
-
-
-
-
+        return torch.matmul(channel_weight, (x+(K-Q)))
 
 
 
 if __name__ == '__main__':
-    test_vector = torch.randn((1,3,5,5), dtype=torch.float32)
-    acm = ACModule(in_channels=3)
+    test_vector = torch.randn((1,2,5,5), dtype=torch.float32)
+    acm = ACMBlock(in_channels=test_vector.shape[1])
 
-    mean_test_vector = acm(test_vector)
+    result = acm(test_vector)
+    print(result)
